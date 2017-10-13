d141c73c92693f9142377f04a0eb65980a98d655
MDD <chenshiman@foxmail.com>
2017-10-13 21:34:22 +0800

    修改.gitignore

--- src/components/MovieDetail.vue
+++ src/components/MovieDetail.vue
@@ -1,39 +1,44 @@
 <template>
 	<div>
-		<card class="movie-detail">
-			<div slot="header">
-				<h2>电影名：{{dataList.title}} ({{dataList.year}})</h2>
-				<h4 class="head-style">原名：{{dataList.original_title}}</h4>
-				<h4 class="head-style">又名：{{dataList.anotherName}}</h4>
-				<p class="head-style">国家：{{dataList.countries}}</p>
-				<p class="head-style">类型：{{dataList.genres}}</p>
-			</div>
-			<div slot="content" class="content">
-				<div class="up">
-					<h3>内容介绍</h3>
+		<scroller lock-x :height='heightData' ref="scrollerEvent">
+			<card class="movie-detail">
+				<div slot="header">
+					<h2>电影名：{{dataList.title}} ({{dataList.year}})</h2>
+					<h4 class="head-style">原名：{{dataList.original_title}}</h4>
+					<h4 class="head-style">又名：{{dataList.aka}}</h4>
+					<p class="head-style">国家：{{dataList.countries}}</p>
+					<p class="head-style">类型：{{dataList.genres}}</p>
 				</div>
-				<div class="down">
-					<div class="left">
-						<img class="previewer-demo-img" :src="dataList.images && dataList.images.medium" @click="show()">
+				<div slot="content" class="content">
+					<div class="up line">
+						<h3>内容介绍</h3>
 					</div>
-					<div class="right">
-						详情：{{dataList.summary}}
-					</div>
-					<div v-transfer-dom>
-						<previewer :list="previewerList" ref="previewer" :options="options"></previewer>
+					<div class="down">
+						<div class="left">
+							<img class="previewer-demo-img" :src="dataList.images && dataList.images.medium" @click="show()">
+						</div>
+						<div class="right" :class="{'hide-text': isTextHide}" @click="changeTextState">
+							详情：{{dataList.summary}}
+						</div>
+						<div v-transfer-dom>
+							<previewer :list="previewerList" ref="previewer" :options="options"></previewer>
+						</div>
 					</div>
 				</div>
-			</div>
-			<div slot="footer" class="footer">
-				尾部
-			</div>
-		</card>
+				<div slot="footer" class="footer">
+					<div class="rating line">
+						<h3>评分信息</h3>
+
+					</div>
+				</div>
+			</card>
+		</scroller>
 	</div>
 </template>
 
 <script>
 import { jsonp } from '../assets/js/common.js';
-import { Card, Previewer, TransferDom, CellBox } from 'vux';
+import { Card, Previewer, TransferDom, CellBox, Scroller } from 'vux';
 
 export default {
 	created() {
@@ -45,7 +50,7 @@ export default {
 				src: res.images.large
 			}];
 			this.dataList = res;
-			this.dataList.anotherName = res.aka.join('、');
+			this.dataList.aka = res.aka.join('、');
 			this.dataList.countries = res.countries.join('、');
 			this.dataList.genres = res.genres.join('、');
 		}).catch(ex => {
@@ -53,14 +58,20 @@ export default {
 		});
 	},
 	methods: {
+		// previwer方法
 		show() {
 			this.$refs.previewer.show(0);
+		},
+		// 改变描述信息的展示状态
+		changeTextState() {
+			this.isTextHide = !this.isTextHide;
 		}
 	},
 	data() {
 		return {
 			dataList: [],
 			previewerList: [],
+			isTextHide: true,
 			options: {
 				getThumbBoundsFn(index) {
 					// find thumbnail element
@@ -81,11 +92,13 @@ export default {
 	components: {
 		Card,
 		Previewer,
-		CellBox
+		CellBox,
+		Scroller
 	},
 	directives: {
 		TransferDom
 	},
+	props: ['heightData']
 }
 </script>
 
@@ -99,10 +112,6 @@ export default {
 		margin-top: 10rem / @param;
 	}
 	.content {
-		.up{
-			padding-bottom: 8px;
-			border-top: 1px solid @lineColor;
-		}
 		.down {
 			display: flex;
 			.left {
@@ -111,15 +120,28 @@ export default {
 			.right {
 				flex: 1;
 				font-size: 14px;
-				overflow: hidden;
 			}
 		}
 	}
+}
+
+.hide-text {
+	overflow: hidden;
+	text-overflow: ellipsis;
+	height: 130px;
+	display: -webkit-box;
+	-webkit-line-clamp: 6;
+	-webkit-box-orient: vertical;
 }
 
 .head-style {
 	color: #555;
 	font-weight: 700;
 	font-size: 16px;
+}
+
+.line {
+	border-top: 1px solid @lineColor;
+	padding-bottom: 8px;
 }
 </style>
